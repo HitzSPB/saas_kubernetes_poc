28916e1ef2817841dc90ba4e09a687bce65e2db7
HitzSPB <daniel.oesterbro@gmail.com>
2021-12-05 16:08:08 +0100

    Added a few log messages

--- src/TeamTwo.WebShop.OrderService.API/Controllers/OrderController.cs
+++ src/TeamTwo.WebShop.OrderService.API/Controllers/OrderController.cs
@@ -20,30 +20,35 @@ namespace TeamTwo.WebShop.OrderService.API.Controllers
 		[HttpGet(Name = "GetAllOrders")]
 		public async Task<IEnumerable<OrderDto>> Get()
 		{
+			_logger.LogInformation("Order Api Controller have been called in the Get All route");
 			return await _orderService.GetAllOrdersAsync();
 		}
 
 		[HttpGet(template: "{id}", Name = "GetSingleOrder")]
 		public async Task<OrderDto> Get(int id)
 		{
+			_logger.LogInformation("Order Api Controller have been called in the Get specific id");
 			return await _orderService.GetOrderByIdAsync(id);
 		}
 
 		[HttpPost(Name = "PostOrder")]
 		public async Task<OrderDto> Post([FromBody] OrderDto orderDto)
 		{
+			_logger.LogInformation("Order Api Controller have been called with the post route");
 			return await _orderService.CreateOrderAsync(orderDto);
 		}
 
 		[HttpPatch(template: "{id}", Name = "UpdateOrder")]
 		public async Task<OrderDto> Patch(int id, [FromBody] OrderDto orderDto)
 		{
+			_logger.LogInformation("Order Api Controller have been called with the update route with id {id}", id);
 			return await _orderService.UpdateOrderAsync(id, orderDto);
 		}
 
 		[HttpDelete(template: "{id}", Name = "DeleteOrder")]
 		public async Task Delete(int id)
 		{
+			_logger.LogInformation("Order Api Controller have been called with the delete route with id {id}", id);
 			await _orderService.DeleteOrderAsync(id);
 		}
 	}
