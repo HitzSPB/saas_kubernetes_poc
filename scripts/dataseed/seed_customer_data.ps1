44da898591ae4da6451b8f93c5c8a078e900396d
HitzSPB <daniel.oesterbro@gmail.com>
2022-01-03 23:20:36 +0100

    Made seeding data from powershell script easier to extend

--- scripts/dataseed/seed_customer_data.ps1
+++ scripts/dataseed/seed_customer_data.ps1
@@ -1,40 +1,13 @@
-$collectionWithItems = New-Object System.Collections.ArrayList
-$customer1 = New-Object System.Object
-$customer1 | Add-Member -MemberType NoteProperty -Name "id" -Value "0"
-$customer1 | Add-Member -MemberType NoteProperty -Name "firstName" -Value "Daniel"
-$customer1 | Add-Member -MemberType NoteProperty -Name "lastName" -Value "Osterbro"
-$customer1 | Add-Member -MemberType NoteProperty -Name "age" -Value "32"
-$collectionWithItems.Add($customer1) | Out-Null
+$data = @("Daniel Osterbro", "Karla West", "Simon Mania", "Barry West", "Carry Angie", "Anna Tiara", "Tina Miarage", "Amin Simonsen", "Wallace Janit", "Silvia Jensen", "Tobias Olafsen", "Valentina Fernandez", "Maria Fernandez", "Fernando Diaz", "Lorren Tamal")
 
-$customer2 = New-Object System.Object
-$customer2 | Add-Member -MemberType NoteProperty -Name "id" -Value "0"
-$customer2 | Add-Member -MemberType NoteProperty -Name "firstName" -Value "Henry"
-$customer2 | Add-Member -MemberType NoteProperty -Name "lastName" -Value "Tar"
-$customer2 | Add-Member -MemberType NoteProperty -Name "age" -Value "22"
-$collectionWithItems.Add($customer2) | Out-Null
-
-$customer3 = New-Object System.Object
-$customer3 | Add-Member -MemberType NoteProperty -Name "id" -Value "0"
-$customer3 | Add-Member -MemberType NoteProperty -Name "firstName" -Value "Maria"
-$customer3 | Add-Member -MemberType NoteProperty -Name "lastName" -Value "Sun"
-$customer3 | Add-Member -MemberType NoteProperty -Name "age" -Value "11"
-$collectionWithItems.Add($customer3) | Out-Null
-
-$customer4 = New-Object System.Object
-$customer4 | Add-Member -MemberType NoteProperty -Name "id" -Value "0"
-$customer4 | Add-Member -MemberType NoteProperty -Name "firstName" -Value "Arne"
-$customer4 | Add-Member -MemberType NoteProperty -Name "lastName" -Value "Simonsen"
-$customer4 | Add-Member -MemberType NoteProperty -Name "age" -Value "34"
-$collectionWithItems.Add($customer4) | Out-Null
-
-foreach($item in $collectionWithItems)
+foreach($item in $data)
 {
   $JSON = @"
   {
-    "id": $($item.id),
-    "firstName": `"$($item.firstName)`",
-    "lastName": "$($item.lastName)",
-    "age": $($item.age)
+    "id": 0,
+    "firstName": `"$($item.Split(" ")[0])`",
+    "lastName": "$($item.Split(" ")[1])",
+    "age": $(Get-Random -Minimum 18 -Maximum 50)
   }
 "@
 Write-Host $JSON
